c6177a86c7d5edf2b5b6e0cf22cfdb2cb397827b
bvenceslas <bvenceslas@gmail.com>
2020-11-17 15:34:29 +0200

    Create Register EndPoint

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,60 @@
 const router = require('express').Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // API test
 router.route('/test').get((req, res) => {
     res.send('JoSign API works perfectly ......');
+});
+
+// register
+router.route('/register').get(async (req, res) => {
+    
+    const { name, email, phone, password } = req.body;
+    // validation
+    if(!name || !email || !phone || !password){
+        res.status(400).json({message: "Completez les champs vides"});
+    }
+
+    User.findOne({email})
+        .then(mail => {
+            if(mail){
+                res.json({message: "User already exists"});
+            }
+            else{
+                const newUser = new User({name, email, phone,password});
+
+                // chiffrer le password
+                bcrypt.genSalt(10, (err, salt) => {
+                    if(err) throw err;
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash; // hash contain the hashed password
+
+                        newUser.save()
+                            .then(user => {
+                                jwt.sign(
+                                    {id: user.id},
+                                    config.get('Venceslas-joshua'),
+                                    {expiresIn: 3600},
+                                    (err, token) => {
+                                        res.json({
+                                            token,
+                                            user:{
+                                                name: user.name,
+                                                email: user.email
+                                            }
+                                        });
+                                    }
+                                )
+                            })
+                            .catch(err => res.json(err));
+                    });
+                });
+            }
+        })
+        .catch(err => res.json(err));
 });
 
 module.exports = router;
